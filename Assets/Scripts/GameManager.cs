3bfe2e1a108faed81e260e514bc222674db0c33b
pedroaugustosgs <pedrovilelasgs@unifei.edu.br>
2024-04-14 16:00:21 -0300

    gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,69 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int vidas = 3;
+    private int score = 0;
+    private float timer = 60.0f; // Timer de 1 minuto
+    public int roupaAtual = 0;
+    public int roupaCorreta = 0; // 0 = Azul, 1 = Verde, 2 = Vermelho
+    public GameObject Player;
+    private float originalSpeed;
+    private float buffDuration = 5.0f; // Duração do buff em segundos
+
     void Start()
     {
-        
+        originalSpeed = Player.GetComponent<PlayerMovement>().speed;
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        vidas = 3;
+        score = 0;
+        roupaCorreta = Random.Range(0, 3);  // 0, 1, 2
+        Debug.Log(roupaCorreta);
+        //  
+    }
+
+    public void RoupaCompleta()
+    {
+        //
+        score++;
+        roupaCorreta = Random.Range(0, 3);  // 0, 1, 2
+    }
+
+    public void RoupaFalha()
+    {
+        //
+        vidas--; 
+        if (vidas <= 0)
+        {
+            // Game Over
+        }
+        roupaCorreta = Random.Range(0, 3);  // 0, 1, 2
+    }
+    
+    public void buff(){
+        Player.GetComponent<PlayerMovement>().speed *= 1.2f; // Aumenta a velocidade em 20%
+        Invoke("removeBuff", buffDuration); // Remove o buff após buffDuration segundos
+    }
+
+    void removeBuff()
+    {
+        Player.GetComponent<PlayerMovement>().speed = originalSpeed; // Restaura a velocidade original
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime; // Decrementa o timer
+
+        if (timer <= 0) // Se o timer acabou
+        {
+            RoupaFalha(); // Chama a função RoupaFalha
+            timer = 60.0f; // Reinicia o timer
+        }
     }
+
+
 }
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -2,27 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Script para spawnar os barris
 public class Spawn : MonoBehaviour
 {
-    public GameObject barrelPrefab; // Prefab do barril
-    public float spawnTimeMin = 2.0f; // Tempo entre os spawns
-    public float spawnTimeMax = 5.0f; // Tempo entre os spawns
-    public float spawnDelay = 2.0f; // Tempo para começar a spawnar
+    public GameObject[] barrelPrefab;
+    public GameObject gm;
+    public float spawnTimeMin = 2.0f;
+    public float spawnTimeMax = 5.0f;
+    public float spawnDelay = 2.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         Spawner();
     }
 
-    // Update is called once per frame
     void Spawner()
     {
-        
-        Instantiate(barrelPrefab, transform.position, Quaternion.identity);
-        
-        Invoke("Spawner", Random.Range(spawnTimeMin, spawnTimeMax));
+        int roupaCorreta = gm.GetComponent<GameManager>().roupaCorreta;
 
+        // Cria uma lista de índices de barris possíveis
+        List<int> possibleBarrels = new List<int>();
+        for (int i = 0; i < barrelPrefab.Length; i++)
+        {
+            if (i == roupaCorreta)
+            {
+                // Adiciona o índice do barril correto uma vez
+                possibleBarrels.Add(i);
+            }
+            else
+            {
+                // Adiciona os índices dos outros barris duas vezes
+                possibleBarrels.Add(i);
+                possibleBarrels.Add(i);
+            }
+        }
+
+        // Escolhe um índice aleatório da lista de barris possíveis
+        int randomBarrel = possibleBarrels[Random.Range(0, possibleBarrels.Count)];
+
+        Instantiate(barrelPrefab[randomBarrel], transform.position, Quaternion.identity);
+
+        Invoke("Spawner", Random.Range(spawnTimeMin, spawnTimeMax));
     }
 }
