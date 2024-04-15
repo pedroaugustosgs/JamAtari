c0fa5792eab4b4d7d43dab8fc113901c2c0f39f0
pedroaugustosgs <pedrovilelasgs@unifei.edu.br>
2024-04-14 21:19:59 -0300

    altas alteracoes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
+using TMPText = TMPro.TextMeshProUGUI;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,14 +19,42 @@ public class GameManager : MonoBehaviour
     public GameObject Player;
     private float originalSpeed;
     private float buffDuration = 5.0f; // Duração do buff em segundos
+    public Sprite roupaAzul;
+    public Sprite roupaVerde;
+    public Sprite roupaVermelha;
+
+    public GameObject linha;
+    public GameObject Roupa;
+    public GameObject scoreCanvas;
+    public GameObject timerCanvas;
+    public GameObject vida1;
+    public GameObject vida2;
+    public GameObject vida3;
+
+    private Image linhaUI = null;
+    private Image RoupaUI = null;
+    private TMPText scoreUI = null;
+    private TMPText timerUI = null;
+
+    public Sprite linhaAzul;
+    public Sprite linhaVerde;
+    public Sprite linhaVermelha;
 
     public int aneisTotal = 4;
     public int aneisPassados = 0;
 
+
+
     void Start()
     {
+        linhaUI = linha.GetComponent<Image>();
+        RoupaUI = Roupa.GetComponent<Image>();
+        scoreUI = scoreCanvas.GetComponent<TMPText>();
+        timerUI = timerCanvas.GetComponent<TMPText>();  
+
         originalSpeed = Player.GetComponent<PlayerMovement>().speed;
         NewRoupa();
+        UpdateUI();
     }
 
     public void NewRoupa()
@@ -30,6 +63,7 @@ public class GameManager : MonoBehaviour
         roupaAtual = Random.Range(0, 3);  // 0, 1, 2
         Debug.Log(roupaCorreta);
         //  
+        UpdateUI();
     }
 
     public void RoupaCompleta()
@@ -37,6 +71,7 @@ public class GameManager : MonoBehaviour
         //
         score++;
         roupaCorreta = Random.Range(0, 3);  // 0, 1, 2
+        UpdateUI();
     }
 
     public void RoupaFalha()
@@ -45,6 +80,7 @@ public class GameManager : MonoBehaviour
         
         roupaCorreta = Random.Range(0, 3);  // 0, 1, 2
         //restartar o timer
+        UpdateUI();
         
 
     }
@@ -58,11 +94,13 @@ public class GameManager : MonoBehaviour
             Debug.Log("Ganhou");
             RoupaCompleta();
         }
+        UpdateUI();
     }
 
     public void buff(){
         Player.GetComponent<PlayerMovement>().speed *= 1.2f; // Aumenta a velocidade em 20%
         Invoke("removeBuff", buffDuration); // Remove o buff após buffDuration segundos
+        UpdateUI();
     }
 
     void removeBuff()
@@ -87,7 +125,63 @@ public class GameManager : MonoBehaviour
             }
         
         }
+        UpdateUI();
     }
 
+    void UpdateUI()
+    {
+        if (roupaAtual == 0)
+        {
+            linhaUI.GetComponent<Image>().sprite = linhaAzul;
+        }
+        else if (roupaAtual == 1)
+        {
+            linhaUI.GetComponent<Image>().sprite = linhaVerde;
+        }
+        else if (roupaAtual == 2)
+        {
+            linhaUI.GetComponent<Image>().sprite = linhaVermelha;
+        }
+        // Atualiza a UI
+        if (roupaCorreta == 0)
+        {
+            RoupaUI.GetComponent<Image>().sprite = roupaAzul;
+        }
+        else if (roupaAtual == 1)
+        {
+            RoupaUI.GetComponent<Image>().sprite = roupaVerde;
+        }
+        else if (roupaAtual == 2)
+        {
+            RoupaUI.GetComponent<Image>().sprite = roupaVermelha;
+        }
 
+        scoreUI.text = "Score: " + score;
+        timerUI.text = "Timer: " + timer.ToString("F0");
+
+        if (vidas == 3)
+        {
+            vida1.SetActive(true);
+            vida2.SetActive(true);
+            vida3.SetActive(true);
+        }
+        else if (vidas == 2)
+        {
+            vida1.SetActive(true);
+            vida2.SetActive(true);
+            vida3.SetActive(false);
+        }
+        else if (vidas == 1)
+        {
+            vida1.SetActive(true);
+            vida2.SetActive(false);
+            vida3.SetActive(false);
+        }
+        else if (vidas == 0)
+        {
+            vida1.SetActive(false);
+            vida2.SetActive(false);
+            vida3.SetActive(false);
+        }
+    }
 }
