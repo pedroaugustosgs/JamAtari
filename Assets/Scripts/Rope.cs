0c736363542e5d26243464e5e2e3d06a18d03803
Lup38 <105895723+Lup38@users.noreply.github.com>
2024-05-18 12:34:37 -0300

    começo script corda

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Rigidbody2D hook;
+    public GameObject prefabRopeSeg;
+    public int numLinks = 0;
+
+    public HingeJoint2D top;
+
+    private float distanceAnchors =0;
+    public Rigidbody2D anchor2;
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    private void FixedUpdate()
+    {
+        distanceAnchors = Vector2.Distance(hook.transform.position, anchor2.transform.position);
+        Debug.Log(distanceAnchors);
+        Debug.Log(numLinks);
+        if (numLinks < (Mathf.Abs((distanceAnchors)*2) + 7))
+        {
+            numLinks++;
+            addLink();
+            Debug.Log("Adicionou");
+        }
+        else if(numLinks > (Mathf.Abs((distanceAnchors)*2) + 9))
+        {
+            removeLink();
+            Debug.Log("Removeu");
+            numLinks--;
+        }
+    }
+
+
+
+    void GenerateRope()
+    {
+        
+            Rigidbody2D prevBod = hook;
+            for (int i = 0; i < numLinks; i++)
+            {
+                GameObject newSeg = Instantiate(prefabRopeSeg);
+                newSeg.transform.parent = transform;
+                newSeg.transform.position = transform.position;
+                HingeJoint2D hj = newSeg.GetComponent<HingeJoint2D>();
+                hj.connectedBody = prevBod;
+
+                prevBod = newSeg.GetComponent<Rigidbody2D>();
+
+                if (i == 0)
+                {
+                    top = hj;
+                }
+                if( i == numLinks - 1)
+                {
+               
+                anchor2.GetComponent<HingeJoint2D>().connectedBody = prevBod;
+                }
+            }
+          
+
+    }
+    
+    public void addLink()
+    {
+        GameObject newLink = Instantiate(prefabRopeSeg);
+        newLink.transform.parent = transform;
+        newLink.transform.position = transform.position;
+        HingeJoint2D hj = newLink.GetComponent<HingeJoint2D>();
+        hj.connectedBody = hook;
+        newLink.GetComponent<RopeSegment>().connectedBelow = top.gameObject;
+        top.connectedBody = newLink.GetComponent<Rigidbody2D>();
+        top.GetComponent<RopeSegment>().ResetAnchor();
+        top = hj;
+
+    }
+
+    public void removeLink()
+    {
+        HingeJoint2D newTop = top.gameObject.GetComponent<RopeSegment>().connectedBelow.GetComponent<HingeJoint2D>();
+        newTop.connectedBody = hook;
+        newTop.gameObject.transform.position = hook.gameObject.transform.position;
+        newTop.GetComponent<RopeSegment>().ResetAnchor();
+        Destroy(top.gameObject);
+        top = newTop;
+    }
+
+}
---
+++ Assets/Scripts/RopeSegment.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSegment : MonoBehaviour
+{
+    public GameObject connectedAbove, connectedBelow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       ResetAnchor();
+
+    }
+
+
+   public void ResetAnchor()
+    {
+        connectedAbove = GetComponent<HingeJoint2D>().connectedBody.gameObject;
+        RopeSegment aboveSegment = connectedAbove.GetComponent<RopeSegment>();
+        if (aboveSegment != null)
+        {
+            aboveSegment.connectedBelow = gameObject;
+            float spriteBottom = connectedAbove.GetComponent<SpriteRenderer>().bounds.size.y;
+            float spriteScale = connectedAbove.GetComponent<Transform>().localScale.y;
+         
+            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, (-spriteBottom / spriteScale));
+        }
+        else
+        {
+            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, 0);
+        }
+    }
+   
+}
